f6cb9bec84a60af7f6323da1f7bebb13e06293e5
BoboRett <jridley246@hotmail.co.uk>
2019-06-30 17:01:49 +0100

    First Build

---
+++ docs/precache-manifest.6c3d88e12e6aa49f020ad6f5bc8873fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5cd56eb9968cff042c7dd1ab845b5c7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19fa203aae8e62bdbbee",
+    "url": "./static/css/main.24ef6273.chunk.css"
+  },
+  {
+    "revision": "eb73085058dbd7de64d3",
+    "url": "./static/js/2.699d068c.chunk.js"
+  },
+  {
+    "revision": "19fa203aae8e62bdbbee",
+    "url": "./static/js/main.32d0ccc3.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
